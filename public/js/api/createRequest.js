a0d40e61b5917a333d8b7c0fc7271c738d3e6be7
AlexAnishin <sinka_6799@mail.ru>
2022-10-21 12:45:26 +0300

    progress///

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,46 @@
 /**
+ * Отправляет AJAX GET запрос на сервер
+ */
+const createGetRequest = (url, data, callback) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+/**
+ * Отправляет AJAX POST запрос на сервер
+ */
+const createPostRequest = (url, responseType, data, callback) => {
+    const formData = new FormData();
+    if (!!data) {
+        for (const [key, value] of Object.entries(data)) {
+            formData.append(key, value);
+        }
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = responseType;
+    xhr.open('POST', url);
+    xhr.onload = function() {
+        let error = null;
+        if (xhr.status !== 200) {
+            error = new Error(xhr.response.statusText);
+        } else if (!xhr.response.success) {
+            alert(xhr.response.error);
+        }
+        callback(error, xhr.response);
+    };
+    xhr.send(formData);
+}
+
+/**
  * Основная функция для совершения запросов
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
+    if (options.method === 'GET') {
+        createGetRequest(options.url, options.data, options.callback);
+    }
+    if (options.method === 'POST') {
+        createPostRequest(options.url, options.responseType, options.data, options.callback);
+    }
 };
